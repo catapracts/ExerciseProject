1e715491e5acbac6cc22107f2edbab67fda44644
catapracts <catapracts@gmail.com>
2025-08-19 22:02:26 +0900

    Android & IOS APP

---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by MacBook on 2025/08/19.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
