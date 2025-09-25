9e73cda66a2055ad8ca0d0337c3559dca2067884
catapracts <catapracts@gmail.com>
2025-09-25 23:07:13 +0900

    certificate JAVA ex

---
+++ certificate/JAVA/ex4.java
@@ -0,0 +1,27 @@
+package JAVA;
+
+class Calculate {
+    public int cal(int a, int b) {
+        return a - b;
+    }
+
+    public float cal(float a, float b) {
+        return a - b;
+    }
+
+    public double cal(double a, double b) {
+        return a + b;
+    }
+
+    public int cal(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class ex4 {
+    public static void main(String[] args) {
+        Calculate a = new Calculate();
+        System.out.print( a.cal(31, 69, 25) + ", " );
+        System.out.println( a.cal(24.8, 5.1) );
+    }
+}
