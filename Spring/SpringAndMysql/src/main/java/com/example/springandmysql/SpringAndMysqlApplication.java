c6a12afecd137fa2517f4946b3f010583b8cf9db
catapracts <catapracts@gmail.com>
2025-10-03 21:46:44 +0900

    Spring & MySQL

---
+++ Spring/SpringAndMysql/src/main/java/com/example/springandmysql/SpringAndMysqlApplication.java
@@ -0,0 +1,16 @@
+package com.example.springandmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMysqlApplication {
+	public SpringAndMysqlApplication() {
+		
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMysqlApplication.class, args);
+	}
+
+}
