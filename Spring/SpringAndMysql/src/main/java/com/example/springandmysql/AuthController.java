6a887eadadb07de0c69308da975a811cc3a578dc
catapracts <catapracts@gmail.com>
2025-10-11 20:45:02 +0900

    JWT response

--- Spring/SpringAndMysql/src/main/java/com/example/springandmysql/AuthController.java
+++ Spring/SpringAndMysql/src/main/java/com/example/springandmysql/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springandmysql.DataForm.LoginRequest;
+import com.example.springandmysql.JWT.JwtResponse;
 import com.example.springandmysql.JWT.JwtTokenUtil;
 
 @RestController
---
+++ Spring/SpringAndMysql/src/main/java/com/example/springandmysql/JWT/JwtResponse.java
@@ -0,0 +1,18 @@
+package com.example.springandmysql.JWT;
+
+public class JwtResponse {
+	
+    private String jwt;
+
+    public JwtResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
