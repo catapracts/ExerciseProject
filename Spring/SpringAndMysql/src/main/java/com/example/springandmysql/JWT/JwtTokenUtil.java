da56b37fbe97cdbb44b59ba3780221a1f2782399
catapracts <catapracts@gmail.com>
2025-10-11 19:35:18 +0900

    JWT Configuration

--- Spring/SpringAndMysql/src/main/java/com/example/springandmysql/JWT/JwtTokenUtil.java
+++ Spring/SpringAndMysql/src/main/java/com/example/springandmysql/JWT/JwtTokenUtil.java
@@ -1,28 +1,38 @@
 package com.example.springandmysql.JWT;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenUtil {
 
-	private final String jwtSecret = "";
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 	private final long jwtExpirationMs = 86400000;
+	private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
 	
 	public String generateToken(UserDetails userDetails) {
 		return Jwts.builder()
 				.setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.signWith(getSigningKey(), SignatureAlgorithm.HS512)
 				.compact();
 	}
 	
 	public String getUsernameFromToken(String token) {
-		return Jwts.parser()
-				.setSigningKey(jwtSecret)
+		return Jwts.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
 				.parseClaimsJws(token)
 				.getBody()
 				.getSubject();	
@@ -30,12 +40,15 @@ public class JwtTokenUtil {
 	
 	public boolean validateToken(String token) {
 		try {
-			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+			Jwts.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token);
 			return true;
 		} catch (JwtException | IllegalArgumentException e) {
-			
+			System.err.println("JWT 검증 실패: " + e.getMessage());
+            return false;
 		}
 		
-		return false;
 	}
 }
